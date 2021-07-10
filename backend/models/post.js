b1704689df2df94ab41d09bc1a5a90044e6a9b8f
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-07-10 10:25:24 +0100

    creat a post instance

--- backend/models/post.js
+++ backend/models/post.js
@@ -2,7 +2,7 @@ const mongoos = require('mongoose');
 
 const postSchema = mongoos.Schema({
     title: { type: String, required: true},
-    title: { type: String, required: true},
+    content: { type: String, required: true},
 });
 
 module.exports = mongoos.model('Post', postSchema);
