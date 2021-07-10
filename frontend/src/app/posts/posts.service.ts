6ec76e2e4e04dd2a9ab329e88d7ebc5b992b954e
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-07-10 11:41:57 +0100

    Transforming Response Data.

--- frontend/src/app/posts/posts.service.ts
+++ frontend/src/app/posts/posts.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import { map, take } from 'rxjs/operators';
 import { Post } from './post.model';
 
 @Injectable({
@@ -13,11 +14,21 @@ export class PostsService {
 
   getPosts() {
     this.http
-      .get<{ message: string; posts: Post[] }>(
-        'http://localhost:3000/api/posts'
+      .get<{ message: string; posts: any }>('http://localhost:3000/api/posts')
+      .pipe(
+        take(1),
+        map((postData) => {
+          return postData.posts.map((post: { title: any; content: any; _id: any; }) => {
+            return {
+              title: post.title,
+              content: post.content,
+              id: post._id,
+            };
+          });
+        })
       )
-      .subscribe((postData) => {
-        this.posts = postData.posts;
+      .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postUpdated.next([...this.posts]);
       });
   }
@@ -38,6 +49,5 @@ export class PostsService {
         this.posts.push(post);
         this.postUpdated.next([...this.posts]);
       });
-
   }
 }
