e5dfcfdb886da14f9e36789f45480d2f426229e5
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-07-12 18:59:06 +0100

    install jsonwebtoken Implementing SPA Authentication

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jsonwebtoken = require("jsonwebtoken");
 const User = require("../models/user");
+const user = require("../models/user");
 
 const router = express.Router();
 
@@ -25,5 +27,34 @@ router.post("/signup", (req, res, next) => {
       });
   });
 });
-
+router.post("/login", (req, res, next) => {
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      bcrypt.compare(req.body.password, user.password);
+    })
+    .then((result) => {
+      if (!result) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      const token = jsonwebtoken.sign(
+        { email: user.email, userId: user._id },
+        "secret_this_should_be_longer",
+        {
+          expiresIn: "1h",
+        }
+      );
+    })
+    .catch((err) => {
+      return res.status(501).json({
+        error: err,
+      });
+    });
+});
 module.exports = router;
