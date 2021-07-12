c1aa544b34f3edabf8eb3a35f9632082272fa9d7
Mohamed-Ali-K <kenis.mohamed.ali@gmail.com>
2021-07-12 23:34:40 +0100

    Adding the Token to Authenticate Requests. creat New interceptor For Http  and add the token to it

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -24,6 +24,7 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthInterceptor } from './auth/auth-interceptor';
 
 
 
@@ -55,7 +56,7 @@ import { SignupComponent } from './auth/signup/signup.component';
     MatPaginatorModule,
     FormsModule
   ],
-  providers: [],
+  providers: [{provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -6,17 +6,23 @@ import { AuthData } from './auth-data.model';
   providedIn: 'root',
 })
 export class AuthService {
+  private token: string;
   constructor(private http: HttpClient) {}
+
+  getToken() {
+    return this.token;
+  }
 
   creatUser(email: string, password: string) {
     const authData: AuthData = {
       email,
       password,
     };
-    this.http.post('http://localhost:3000/api/user/signup',authData).subscribe(response =>{
-      console.log(response);
-
-    })
+    this.http
+      .post('http://localhost:3000/api/user/signup', authData)
+      .subscribe((response) => {
+        console.log(response);
+      });
   }
 
   login(email: string, password: string) {
@@ -24,8 +30,11 @@ export class AuthService {
       email,
       password,
     };
-    this.http.post('http://localhost:3000/api/user/login',authData).subscribe(response =>{
-      console.log(response);
-    })
+    this.http
+      .post<{ token: string }>('http://localhost:3000/api/user/login', authData)
+      .subscribe((response) => {
+        const token = response.token;
+        this.token = token;
+      });
   }
 }
